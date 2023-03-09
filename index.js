03acddefb3739c8ff594925456432bf73dd35d58
richiegnuez261987 <richiegnuez261987@gmail.com>
2023-03-09 05:12:17 -0800

    Undo JQuery

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+
+$(document).ready(function(){
+  $.ajax({
+      url:'navigator.html',
+  
+      caches:false,
+      success:function(data){
+        $("#navContainer").html(data);
+      }
+    })
+  $.ajax({
+  url:'footer.html',
+
+  caches:false,
+  success:function(data){
+    $("#footerContainer").html(data);
+  }
+})
+})
+
+
 //FOR DROPDOWN MENU NAVIGATION
 const dropdown = document.getElementById("dropdown");
 const services = document.querySelector(".services");
