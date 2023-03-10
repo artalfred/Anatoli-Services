dcb63c0676cff9a835935c49beea094eb34ab8e9
richiegnuez261987 <richiegnuez261987@gmail.com>
2023-03-10 04:36:38 -0800

    Footer Add to Index

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 
-$(document).ready(function(){
-  $.ajax({
-      url:'navigator.html',
-  
-      caches:false,
-      success:function(data){
-        $("#navContainer").html(data);
-      }
-    })
-  $.ajax({
-  url:'footer.html',
-
-  caches:false,
-  success:function(data){
-    $("#footerContainer").html(data);
-  }
-})
-})
 
 
 //FOR DROPDOWN MENU NAVIGATION
