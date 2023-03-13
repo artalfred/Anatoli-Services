289289d612b395655afdbf0a9ef3bc769cca0edb
ArtAlfred <bernalesartalfred@gmail.com>
2023-03-14 07:16:59 +0800

    fixed link

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const container = document.getElementById("footer");
 const navigation = document.getElementById("navigation");
 
-
 fetch("./navigator.html")
-  .then(response => response.text())
-  .then(html => navigation.innerHTML = html)
-  .catch(error => console.error(error));
-
-
-
+  .then((response) => response.text())
+  .then((html) => (navigation.innerHTML = html))
+  .catch((error) => console.error(error));
 
 fetch("./footer.html")
-  .then(response => response.text())
-  .then(html => container.innerHTML = html)
-  .catch(error => console.error(error));
-
+  .then((response) => response.text())
+  .then((html) => (container.innerHTML = html))
+  .catch((error) => console.error(error));
 
 //FOR DROPDOWN MENU NAVIGATION
 const dropdown = document.getElementById("dropdown");
